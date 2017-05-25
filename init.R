e1c6d22c14b6af94240588dda00e57b9c2e4c00d
Matheus Macedo Lopes <matheusmlopess@gmail.com>
2017-05-25 15:49:18 -0300

    Add files via upload

--- init.R
+++ init.R
@@ -64,10 +64,18 @@ library(shinyBS)
 
   #SEE IF ITS A WINDOWS OR LINUX SYSTEM 
 	if(grepl("Windows",userInfo[1])){
-	 
-	   mainDir <- paste("C:/Users",userInfo[6],"Desktop",sep="/")
-	   subDir  <- "data"
-	  # CHECK IF DATA FOLDER IS ALREADY SET
+
+      mainDir <- paste("C:/Users",userInfo[6],"Desktop",sep="/")
+	    subDir  <- "data"
+	}else{
+	  
+	    dlgMessage(paste("EartWorms will be running in a Linux Distro AWESOME mate!\n", userInfo[1],sistemInfo$platform, sep ="  \n     "))$res
+	    mainDir <- paste("~/home",userInfo[6],"Desktop",sep="/")
+	    subDir  <- "data"
+	  
+	}
+	   
+	    # CHECK IF DATA FOLDER IS ALREADY SET
 	    if (file.exists(file.path(mainDir, subDir))){
 	      
 	         #CHECK IF DATA FILE (CSV) IS IN THE FOLDER ALREADY
@@ -75,7 +83,6 @@ library(shinyBS)
 	              #CHECK IF DATA FOLDER & DATA FILE ARE SET TO GO....
 	              #print("ALL SET: DATA FOLDER AND DATA FILE ON POSITION")
                 dlgMessage("ALL SET \nData folder created in Desktop \nEarthWorms data file (CSV) selected!\n\n\n             INITIALIZING GUI        ")$res
-	             
 	              return(0)
 	            
 	          }else{
@@ -109,56 +116,7 @@ library(shinyBS)
 	    
 	  }
     
-	} else {
-	  
-	 
-	  dlgMessage(paste("EartWorms will be running in a Linux Distro AWESOME mate!\n", userInfo[1],sistemInfo$platform, sep ="  \n     "))$res
-	  mainDir <- paste("~/home",userInfo[6],"Desktop",sep="/")
-	  subDir  <- "data"
-	  
-	  if (file.exists(file.path(mainDir, subDir))){
-	    
-	    #CHECK IF DATA FILE (CSV) IS IN THE FOLDER ALREADY
-	    if(file.exists(file.path(finalDataPath))){
-	      #CHECK IF DATA FOLDER & DATA FILE ARE SET TO GO....
-	      #print("ALL SET: DATA FOLDER AND DATA FILE ON POSITION")
-	      dlgMessage("ALL SET \nData folder created in Desktop \nEarthWorms data file (CSV) selected!\n\n\n             INITIALIZING GUI        ")$res
-	      
-	      return(0)
-	      
-	    }else{
-	      #DO WHATERVER TO CREATE DATA FOLDER & SELECT DATA FILE
-	      #print("ALMOST ALL SET: DATA FOLDER CREATED ALREAD... PLEASE SELECT DATA FILE")
-	      dlgMessage("Data folder created in Desktop... \nNow, Please select the EarthWorms data file (CSV)!")$res
-	      pathToCSVfile <- file.choose()
-	      finalDataPath=paste(mainDir,subDir,"dataFile.csv",sep="/")
-	      move_file(from = pathToCSVfile, to   = finalDataPath )
-	      
-	      #---------------------------------------------------------------------------------------------------
-	      # RAPHA'S VARIABLE TO DATAS FILE CONTENT (send to workspace)
-	      #---------------------------------------------------------------------------------------------------
-	      don <- read.csv(finalDataPath,sep=";",na.string="" , header = T, dec=",", fileEncoding="ISO-8859-1")
-	      
-	    }
-	    
-	  } else {
-	    #DO WHATERVER TO CREATE DATA FOLDER & SELECT DATA FILE
-	    print("Initializing platform for windows user ...")
-	    dlgMessage("Initializing platform for windows user ... Creating Data folder and selecting Data Files (CSV)")$res
-	    dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
-	    pathToCSVfile <- file.choose()
-	    finalDataPath=paste(mainDir,subDir,"dataFile.csv",sep="/")
-	    move_file(from = pathToCSVfile, to = finalDataPath )
-	    
-	    #---------------------------------------------------------------------------------------------------
-	    # RAPHA'S VARIABLE TO DATAS FILE CONTENT (send to workspace)
-	    #---------------------------------------------------------------------------------------------------
-	    don <- read.csv(finalDataPath,sep=";",na.string="" , header = T, dec=",", fileEncoding="ISO-8859-1")
-	    
-	   }
-	  	  
-	}
-
+  
   # SETTING UP SHYNE APP GUI
     runApp("EarthWormsAPP/")
 
